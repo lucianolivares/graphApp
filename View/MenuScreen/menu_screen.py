9cf896c5bb036a6bfe36d5e2343d36b9f6763393
lucianolivares <lucianolivares@gmail.com>
2023-04-17 00:42:37 -0400

    main screen changes

--- View/MenuScreen/menu_screen.py
+++ View/MenuScreen/menu_screen.py
@@ -1,23 +1,34 @@
 from kivymd.uix.screen import MDScreen
 from View.MenuScreen.components import MenuCard
-
+import os
 
 class MenuScreenView(MDScreen):
     def on_enter(self, *args) -> None:
         if not self.ids.menu_list.data:
-            menu_list = [
-                "Main",
-                "Second",
-            ]
-            menu_list.sort()
-            for name_card in menu_list:
+            screens_data = dict(
+                main = dict(
+                    label = "Predicción de Fragmentación",
+                    source = "assets/fragmentación.png"
+                ),
+                second = dict(
+                    label = "Gráfico de Frecuencias",
+                    source = "assets/frecuencía.jpg"
+                ),
+                video = dict(
+                    label = "Reproductor de Video",
+                    source = "frecuencía.jpg"
+                ),
+            )
+            dict(sorted(screens_data.items()))
+            for name, props in screens_data.items():
                 self.ids.menu_list.data.append(
                     {
                         "viewclass": "MenuCard",
-                        "title": name_card,
+                        "title": props['label'],
+                        "image": props['source'],
                         "elevation": 1,
                         "shadow_softness": 4,
-                        "on_release": lambda x=name_card.lower(): self.switch_screen(
+                        "on_release": lambda x=name.lower(): self.switch_screen(
                             x
                         )
                     }
